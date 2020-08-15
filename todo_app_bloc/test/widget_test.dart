adee72e586adf4857d4318b5f3fa928dd5287859
sanadsadhik <56155278+sanadsadhik@users.noreply.github.com>
2020-08-15 14:51:34 +0530

    implemented widget tests

--- todo_app_bloc/test/widget_test.dart
+++ todo_app_bloc/test/widget_test.dart
@@ -1,17 +1,21 @@
-import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:todo_app_bloc/BLoC/todo_bloc.dart';
+import 'package:todo_app_bloc/main.dart';
 
 void main() {
-  group('ToDoBloc', () {
-    blocTest(
-      'emits [] when no events added',
-      build: () => ToDosBloc([]),
-      act: (todosBloc) => todosBloc.add(AddToDo(ToDoBloc('tester todo'))),
-      expect: [
-        [],
-        ['tester todo']
-      ],
-    );
+  testWidgets('todo widget test', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    var textField = find.byType(TextField);
+    expect(textField, findsOneWidget);
+    var button = find.text('ADD TODO');
+    expect(button, findsOneWidget);
+    var list = find.byType(ListView);
+    expect(list, findsOneWidget);
+
+    await tester.enterText(textField, 'tester todo text');
+    expect(find.text('tester todo text'), findsOneWidget);
+    await tester.tap(button);
+    await tester.pump();
+    expect(find.text('tester todo text'), findsOneWidget);
   });
 }
